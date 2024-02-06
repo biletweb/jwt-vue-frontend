9092f8b81aab861fdb25cb99f762eda4eb3c547c
biletweb <bilet.igor@gmail.com>
2024-02-06 13:13:27 +0200

    Changed color of navbar links

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -8,7 +8,7 @@
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="mr-1 h-5 w-5"
+          class="mr-1 h-5 w-5 text-green-500"
         >
           <path
             stroke-linecap="round"
@@ -16,7 +16,9 @@
             d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
           />
         </svg>
-        <router-link to="/">Home</router-link>
+        <router-link to="/" class="text-green-500 hover:text-green-700"
+          >Home</router-link
+        >
       </div>
 
       <div v-if="token" class="flex items-center">
@@ -26,7 +28,7 @@
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="mr-1 h-5 w-5"
+          class="mr-1 h-5 w-5 text-green-500"
         >
           <path
             stroke-linecap="round"
@@ -34,15 +36,29 @@
             d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
           />
         </svg>
-        <router-link to="/users/index">Users</router-link>
+        <router-link
+          to="/users/index"
+          class="text-green-500 hover:text-green-700"
+          >Users</router-link
+        >
       </div>
     </div>
     <div class="mb-5 flex gap-5">
-      <router-link v-if="!token" to="/auth/signin">Sign in</router-link>
-      <router-link v-if="!token" to="/auth/signup">Sign up</router-link>
+      <router-link
+        v-if="!token"
+        to="/auth/signin"
+        class="text-green-500 hover:text-green-700"
+        >Sign in</router-link
+      >
+      <router-link
+        v-if="!token"
+        to="/auth/signup"
+        class="text-green-500 hover:text-green-700"
+        >Sign up</router-link
+      >
       <div v-if="token" class="flex items-center">
         <AppLoader v-if="!user_name" class="h-5 w-5" />
-        <div>
+        <div class="text-green-500">
           {{ user_name }}
         </div>
         <router-link @click="logout" to="/">
@@ -52,7 +68,7 @@
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="ml-1 h-5 w-5"
+            class="ml-1 h-5 w-5 text-green-500 hover:text-green-700"
           >
             <path
               stroke-linecap="round"
