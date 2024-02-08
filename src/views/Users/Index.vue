1d1b0f4d223bc61c0dc2be9a0e05f1bf6d875c8e
biletweb <bilet.igor@gmail.com>
2024-02-08 17:01:58 +0200

    Added email confirmation information to the user list

--- src/views/Users/Index.vue
+++ src/views/Users/Index.vue
@@ -74,6 +74,7 @@
           <th scope="col" class="px-6 py-3">Email</th>
           <th scope="col" class="px-6 py-3">Created</th>
           <th scope="col" class="px-6 py-3">Updated</th>
+          <th scope="col" class="px-6 py-3">Confirmed</th>
           <th scope="col" class="px-6 py-3">Actions</th>
         </tr>
       </thead>
@@ -94,6 +95,38 @@
           <td class="truncate px-6 py-4">{{ user.email }}</td>
           <td class="truncate px-6 py-4">{{ user.created_at }}</td>
           <td class="truncate px-6 py-4">{{ user.updated_at }}</td>
+          <td v-if="user.verify_email" class="truncate px-6 py-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6 text-red-500 opacity-60"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </td>
+          <td v-else class="truncate px-6 py-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-6 w-6 text-green-500 opacity-60"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </td>
           <td class="px-6 py-4">
             <div class="flex gap-3">
               <router-link
